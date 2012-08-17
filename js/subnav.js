3a2d3cfe37e38c87ebdc0d952f04bf7586f950bd
Ceasar Bautista <cbautista2010@gmail.com>
2012-08-16 22:23:40 -0400

    Make the nav-bar white
    I think white matches our brand a little better than black does. It also
    happens to make it much easier to see the PennApps logo.

--- js/subnav.js
+++ js/subnav.js
@@ -1,17 +1,19 @@
 $MASTHEAD = $('#masthead button');
+SIGN_UP_STYLE = 'btn-primary'
+BLEND_IN_STYLE = ''
 
 if ($MASTHEAD.length === 0) {
-    $('#global-nav button').removeClass('btn-inverse');
-    $('#global-nav button').addClass('btn-primary');
+    $('#global-nav button').removeClass(BLEND_IN_STYLE);
+    $('#global-nav button').addClass(SIGN_UP_STYLE);
 } else {
     $(document).scroll(function(){
         masthead_bottom = $MASTHEAD.offset().top
         if ($(this).scrollTop() > masthead_bottom) {
-            $('#global-nav button').removeClass('btn-inverse');
-            $('#global-nav button').addClass('btn-primary');
+            $('#global-nav button').removeClass(BLEND_IN_STYLE);
+            $('#global-nav button').addClass(SIGN_UP_STYLE);
         } else {
-            $('#global-nav button').removeClass('btn-primary');
-            $('#global-nav button').addClass('btn-inverse');
+            $('#global-nav button').removeClass(SIGN_UP_STYLE);
+            $('#global-nav button').addClass(BLEND_IN_STYLE);
         }
     });
 }
